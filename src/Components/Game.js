fa929963837a5355c19fe161b354c732642a3cda
Junchi <wangjunchiseu@outlook.com>
2020-11-17 16:19:40 +0800

    dynamic layout for different size of the game

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../asset/css/dist/style.css'
 import GameInfo from './GameInfo';
 import GameBoard from './GameBoard';
 function Game(){
-    return(
-        <div id='myGame'>
-            <div id='gameTitle'>2048 Online Game</div>
-            <GameInfo></GameInfo>
-            <GameBoard></GameBoard>
-        </div>
-    )
+    const [isNewGame, setNewGame] = useState(true);
+    const [boardSize, setBoardSize] = useState(3);
+    return (
+      <div id="myGame">
+        <div id="gameTitle">2048 Online Game</div>
+        <GameInfo
+          boardSize={boardSize}
+          setNewGame={setNewGame}
+          setBoardSize={setBoardSize}
+        ></GameInfo>
+        <GameBoard isNewGame={isNewGame} boardSize={boardSize}></GameBoard>
+      </div>
+    );
 }
 
 export default Game;
--- src/Components/GameInfo.js
+++ src/Components/GameInfo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function GameInfo(){
+function GameInfo(props){
     return (
         <div id='gameInfo'>
             <div className="score"> Score: </div>
-            <div className="score">Highest Score:</div>
+            <div className="level">Level: {props.boardSize}</div>
         </div>
     )
 }
