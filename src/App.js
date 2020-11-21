1d7383a2a48dac0dd6294af0e55a5bcdd27ed444
Ais0n <aisooon@yahoo.com>
2020-11-21 11:46:18 +0800

    finish: the initialization of the gameboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import * as React from "react";
+import { render } from "react-dom";
 import Game from './Components/Game';
-function App() {
-  return (
-    <div className="App">
-      <Game></Game>
-      <footer> This is the game for the bootcamp in ByteDance</footer>
-    </div>
-  );
+
+var Direction = {
+  left: 0,
+  up: 1,
+  right: 2,
+  down: 3
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9)
+    };
+  }
+
+ 
+
+  render() {
+    return (
+      <div className="App">
+        <Game></Game>
+        <footer> This is the game for the bootcamp in ByteDance</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
