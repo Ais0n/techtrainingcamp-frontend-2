6b73c291cccc50906ace975dd6fc2ddcbd6f34f0
Ais0n <aisooon@yahoo.com>
2020-11-23 23:05:10 +0800

    combine logic and animation

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -15,6 +15,12 @@ function Game() {
   //const [value, setvalue] = useState(2);; // just for test
   const [isLose, setLose] = useState(false);
 
+  var tilesMovement = new Map();
+  //tilesMovement.set("0,0", { newPos: [0, 3], visible: true, isNew: false });
+  //tilesMovement.set("2,2", { newPos: [2, 3], visible: false, isNew: false });
+  //tilesMovement.set("2,3", { newPos: [2, 3], visible: false, isNew: false });
+  var testNewTiles = [];//= [{ position: [2, 3], value: 4 }, { position: [2, 1], value: 2 }];
+
   const addOneNumber = (numbers) => {
     let newNumbers = cloneDeep(numbers);
     let size = newNumbers.length;
@@ -31,6 +37,9 @@ function Game() {
       if (newNumbers[row][col] === 0) {
         // if number is 0, set it to be 2 or 4
         newNumbers[row][col] = Math.random() < 0.7 ? 2 : 4;
+        testNewTiles.push(
+          { position: [col, row], value: newNumbers[row][col] }
+        );
         break;
       }
     }
@@ -48,10 +57,10 @@ function Game() {
       numbers = addOneNumber(numbers);
       numbers = addOneNumber(numbers);
       setGridNumbers(numbers);
-      let newTiles=[];
+      let newTiles = [];
       for (var i = 0; i < boardSize; i++) {
         for (var j = 0; j < boardSize; j++) {
-          if(numbers[i][j]!==0){
+          if (numbers[i][j] !== 0) {
             const newTile = {
               key: uuidv4(),
               value: numbers[i][j],
@@ -81,7 +90,20 @@ function Game() {
     setLose(checkLose());
   }
 
+  /*function checkNew(j, i) {
+    let res = false;
+    for (let item in testNewTiles) {
+      if (item.position == [j, i]) {
+        res = true;
+        break;
+      }
+    }
+    return res;
+  }*/
+
   const mergeGrid = (direction) => {
+    tilesMovement.clear();
+    testNewTiles = [];
     var newGrid = [], addscore = 0;
     switch (direction) {
       case 0: //left
@@ -96,22 +118,25 @@ function Game() {
     }
     var res = (newGrid === gridNumbers);
     if (!res) newGrid = addOneNumber(newGrid);
-    let newTiles=[];
+    /*let newTiles = [];
     for (var i = 0; i < boardSize; i++) {
       for (var j = 0; j < boardSize; j++) {
-        if(newGrid[i][j]!==0){
+        if (newGrid[i][j] !== 0) {
           const newTile = {
             key: uuidv4(),
             value: newGrid[i][j],
             position: [j, i],
-            visible: true
+            visible: true,
+            isNew: checkNew(j, i)
           }
           newTiles.push(newTile);
         }
       }
     }
-    setTiles(newTiles);
-    console.log(newTiles);
+    //setTiles(newTiles);
+    console.log(newTiles);*/
+    console.table(newGrid);
+    updateTiles(tilesMovement, testNewTiles);
     setScore(score + addscore);
     setGridNumbers(newGrid);
   }
@@ -120,11 +145,11 @@ function Game() {
   const mergeRow = (isReverse) => {
     let newGrid = cloneDeep(gridNumbers);
     let addscore = 0;
-    console.log(newGrid);
-    //console.table(gridNumbers);
+    var j, k, left_bound;
     for (let i = 0; i < boardSize; i++) {
       //reverse the row
       if (isReverse) newGrid[i] = newGrid[i].reverse();
+      /*
       //shift the row left
       newGrid[i] = newGrid[i].filter(x => x !== 0);
       for (let j = newGrid[i].length; j < boardSize; j++)newGrid[i][j] = 0;
@@ -139,7 +164,49 @@ function Game() {
           }
           newGrid[i][boardSize - 1] = 0;
         }
+      }*/
+      for ([j, k, left_bound] = [0, -1, 0]; j < boardSize; j++) {
+        if (newGrid[i][j] === 0) continue;
+        if (k === -1) k = j;
+        else if (newGrid[i][k] !== newGrid[i][j]) { //Move
+          newGrid[i][left_bound] = newGrid[i][k];
+          tilesMovement.set(
+            isReverse ? [boardSize - k - 1, i].toString() : [k, i].toString(),
+            { newPos: (isReverse ? [boardSize - left_bound - 1, i] : [left_bound, i]), visible: true, isNew: false }
+          );
+          left_bound++;
+          k = j;
+        }
+        else { //Merge
+          newGrid[i][left_bound] = newGrid[i][j] * 2;
+          tilesMovement.set(
+            isReverse ? [boardSize - k - 1, i].toString() : [k, i].toString(),
+            { newPos: (isReverse ? [boardSize - left_bound - 1, i] : [left_bound, i]), visible: false, isNew: false }
+          );
+          tilesMovement.set(
+            isReverse ? [boardSize - j - 1, i].toString() : [j, i].toString(),
+            { newPos: (isReverse ? [boardSize - left_bound - 1, i] : [left_bound, i]), visible: false, isNew: false }
+          );
+          testNewTiles.push(
+            {
+              position: isReverse ? [boardSize - left_bound - 1, i] : [left_bound, i],
+              value: newGrid[i][left_bound]
+            }
+          );
+          left_bound++;
+          k = -1;
+        }
       }
+      if (k != -1) //Move the final one
+      {
+        newGrid[i][left_bound] = newGrid[i][k];
+        tilesMovement.set(
+          isReverse ? [boardSize - k - 1, i].toString() : [k, i].toString(),
+          { newPos: (isReverse ? [boardSize - left_bound - 1, i] : [left_bound, i]), visible: true, isNew: false }
+        );
+        left_bound++;
+      }
+      for (k = left_bound; k < boardSize; k++)newGrid[i][k] = 0;
       //reverse the row
       if (isReverse) newGrid[i] = newGrid[i].reverse();
     }
@@ -149,6 +216,7 @@ function Game() {
   const mergeColumn = (isReverse) => {
     let newGrid = cloneDeep(gridNumbers);
     let addscore = 0;
+    var j, k, up_bound;
     for (let i = 0; i < boardSize; i++) {
       if (isReverse) {  //Reverse the column
         for (let j = 0; j < boardSize - j - 1; j++) {
@@ -157,6 +225,7 @@ function Game() {
           newGrid[boardSize - 1 - j][i] = tmp;
         }
       }
+      /*
       //Shift the column up
       for (let [j, k] = [0, 0]; j < boardSize; j++) {
         if (newGrid[j][i] !== 0) {
@@ -177,7 +246,49 @@ function Game() {
           }
           newGrid[boardSize - 1][i] = 0;
         }
+      }*/
+      for ([j, k, up_bound] = [0, -1, 0]; j < boardSize; j++) {
+        if (newGrid[j][i] === 0) continue;
+        if (k === -1) k = j;
+        else if (newGrid[k][i] !== newGrid[j][i]) { //Move
+          newGrid[up_bound][i] = newGrid[k][i];
+          tilesMovement.set(
+            isReverse ? [i, boardSize - k - 1].toString() : [i, k].toString(),
+            { newPos: (isReverse ? [i, boardSize - up_bound - 1] : [i, up_bound]), visible: true, isNew: false }
+          );
+          up_bound++;
+          k = j;
+        }
+        else { //Merge
+          newGrid[up_bound][i] = newGrid[j][i] * 2;
+          tilesMovement.set(
+            isReverse ? [i, boardSize - k - 1].toString() : [i, k].toString(),
+            { newPos: (isReverse ? [i, boardSize - up_bound - 1] : [i, up_bound]), visible: false, isNew: false }
+          );
+          tilesMovement.set(
+            isReverse ? [i, boardSize - j - 1].toString() : [i, j].toString(),
+            { newPos: (isReverse ? [i, boardSize - up_bound - 1] : [i, up_bound]), visible: false, isNew: false }
+          );
+          testNewTiles.push(
+            {
+              position: isReverse ? [i, boardSize - up_bound - 1] : [i, up_bound],
+              value: newGrid[up_bound][i]
+            }
+          );
+          up_bound++;
+          k = -1;
+        }
       }
+      if (k != -1) //Move the final one
+      {
+        newGrid[up_bound][i] = newGrid[k][i];
+        tilesMovement.set(
+          isReverse ? [i, boardSize - k - 1].toString() : [i, k].toString(),
+          { newPos: (isReverse ? [i, boardSize - up_bound - 1] : [i, up_bound]), visible: true, isNew: false }
+        );
+        up_bound++;
+      }
+      for (k = up_bound; k < boardSize; k++)newGrid[k][i] = 0;
       if (isReverse) {  //Reverse the column
         for (let j = 0; j < boardSize - j - 1; j++) {
           let tmp = newGrid[j][i];
@@ -219,12 +330,12 @@ function Game() {
     setTiles(curTiles);
     console.log(curTiles);
   }
-  
+
   /**
    * 模拟算法返回的数据
    */
   // 初始化旧的Tiles
-  const createTiles = ()=>{
+  const createTiles = () => {
     const tiles = [];
     // 假设现在是回合1，玩家操作后，tiles的情况如下
     // 包含3中不同类型的tile
@@ -237,34 +348,34 @@ function Game() {
     //  0 0 2 0
     // 新增加的tile
     tiles.push({
-      key : uuidv4(),
+      key: uuidv4(),
       value: 4,
-      position: [0,0],
+      position: [0, 0],
       visible: true,
-      isNew:  true
+      isNew: true
     });
     // 未合并的tile
     tiles.push({
-      key : uuidv4(),
+      key: uuidv4(),
       value: 2,
-      position: [2,2],
+      position: [2, 2],
       visible: true,
-      isNew:  false
+      isNew: false
     });
     tiles.push({
-      key : uuidv4(),
+      key: uuidv4(),
       value: 2,
-      position: [2,3],
+      position: [2, 3],
       visible: true,
-      isNew:  false
+      isNew: false
     });
     // 合并后消失的tile
     tiles.push({
-      key : uuidv4(),
+      key: uuidv4(),
       value: 8,
-      position: [1,1],
+      position: [1, 1],
       visible: false,
-      isNew:  false
+      isNew: false
     });
     setTiles(tiles);
   }
@@ -276,54 +387,52 @@ function Game() {
   /**
    *  模拟算法的返回值
    */
-  const tilesMovement = new Map();  
-  tilesMovement.set("0,0", {newPos: [0,3], visible:true, isNew: false});
-  tilesMovement.set("2,2", {newPos: [2,3], visible:false, isNew: false}); 
-  tilesMovement.set("2,3", {newPos: [2,3], visible:false, isNew: false});
-  const testNewTiles = [{position:[2,3], value:4}, {position:[2,1], value:2}];
+
   /**
    * 根据算法返回的结果对tiles进行更新
    * {tilesMovement}：Map 棋盘上所有可见tile的新位置
    * {testNewTiles}: array 棋盘上新增的tile
    */
-  const updateTiles = (tilesMovement,testNewTiles)=>{
+  const updateTiles = (tilesMovement, testNewTiles) => {
+    console.log(tilesMovement);
+    console.log(testNewTiles);
     // 首先删除visible===false的tile
     // 一定要先删除不可见的tile，再更新剩下的tiles
     // 此时棋盘上不应该有两个可见的tile处于同一位置
-    const allVisibleTiles = tiles.filter(tile=>tile.visible===true);
+    const allVisibleTiles = tiles.filter(tile => tile.visible === true);
     // 其次更新tiles的位置，并将isNew设置为false
-    console.log("allVisibleTiles",allVisibleTiles);
-    const tilesNewPos = allVisibleTiles.map(tile=>{
+    console.log("allVisibleTiles", allVisibleTiles);
+    const tilesNewPos = allVisibleTiles.map(tile => {
       let tileMove = tilesMovement.get(tile.position.toString());
-      tile.position=tileMove.newPos;
-      tile.visible=tileMove.visible;
+      tile.position = tileMove.newPos;
+      tile.visible = tileMove.visible;
       tile.isNew = false;
       return tile;
     });
-    console.log("tilesNewPos",tilesNewPos);
+    console.log("tilesNewPos", tilesNewPos);
     // 添加所有新tiles
-    for(let i=0;i<testNewTiles.length;i++){
+    for (let i = 0; i < testNewTiles.length; i++) {
       tilesNewPos.push({
-        key : uuidv4(),
+        key: uuidv4(),
         value: testNewTiles[i].value,
         position: testNewTiles[i].position,
         visible: true,
-        isNew:  true
+        isNew: true
       })
     }
     // 更新之后一共会用5个tile，其中3个可以在棋盘上见到，剩下的2个会在下一次更新时删掉
     setTiles(tilesNewPos);
   }
 
-  const testState = ()=>{
-    const newTiles = tiles.map((tile)=>{
+  const testState = () => {
+    const newTiles = tiles.map((tile) => {
       return {
         key: tile.key,
         value: tile.value,
-        position: [tile.position[0],boardSize-1],
+        position: [tile.position[0], boardSize - 1],
         visible: false
       }
-    }) 
+    })
     setTiles(newTiles);
   }
   useEffect(() => {
@@ -354,12 +463,12 @@ function Game() {
         setScore={setScore}
         //position={position}
         tiles={tiles}
-        //tileValue={value} // TileValue for test
+      //tileValue={value} // TileValue for test
       ></GameBoard>
       {/* <button onClick={(e) => testState()}>Logic test</button> */}
       {/* <button onClick={(e)=> addTile([0,0],256)}>NewTile!</button> */}
-      <button onClick={(e)=> createTiles()}>Reset tiles</button>
-      <button onClick={(e)=>updateTiles(tilesMovement,testNewTiles)}>Move Down（不要单独按）</button>
+      <button onClick={(e) => createTiles()}>Reset tiles</button>
+      <button onClick={(e) => updateTiles(tilesMovement, testNewTiles)}>Move Down（不要单独按）</button>
     </div>
   );
 }
