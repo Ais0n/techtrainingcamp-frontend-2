2c430b65bc6ca8e8e4f0198e652876507443f246
Junchi <wangjunchiseu@outlook.com>
2020-11-24 22:41:53 +0800

    增加react-router,一个简单的跳转demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Game from './Components/Game';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Game></Game>
-      <footer> This is the game for the bootcamp in ByteDance</footer>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route path="/singleGame">
+            <Game></Game>
+          </Route>
+          <Route path="/">
+            {/* 开始界面可以直接在这里写，但最好还是写成以一个组件的形式 */}
+            <div id=" id="wrapper>
+              <button><Link to="/singleGame">Start Single Game</Link></button>
+            </div>
+          </Route>
+        </Switch>
+        <footer> This is the game for the bootcamp in ByteDance</footer>
+      </div>
+    </Router>
   );
 }
 
